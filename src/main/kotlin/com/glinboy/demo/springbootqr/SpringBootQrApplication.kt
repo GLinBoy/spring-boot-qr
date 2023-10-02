5f608cd96a67ed5083f1b2cbc150c4937b26e548
GLinBoy <hojjatabedie@gmail.com>
2023-10-02 11:58:21 +0200

    🕯 START: Initiated the project
    resolved #1

---
+++ src/main/kotlin/com/glinboy/demo/springbootqr/SpringBootQrApplication.kt
@@ -0,0 +1,11 @@
+package com.glinboy.demo.springbootqr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootQrApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootQrApplication>(*args)
+}
---
+++ src/test/kotlin/com/glinboy/demo/springbootqr/SpringBootQrApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glinboy.demo.springbootqr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootQrApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
