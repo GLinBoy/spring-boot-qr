4ec70d16f8720b5fcc87b2e3cd70cd489eeecf68
GLinBoy <hojjatabedie@gmail.com>
2023-10-02 12:31:12 +0200

    📦 NEW: Added HomeResource

---
+++ src/main/kotlin/com/glinboy/demo/springbootqr/web/controller/HomeResource.kt
@@ -0,0 +1,13 @@
+package com.glinboy.demo.springbootqr.web.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/")
+class HomeResource {
+
+	@GetMapping
+	fun getHome(): String = "index"
+}
